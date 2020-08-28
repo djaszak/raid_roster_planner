b66ceab07c934e960f04a224a0dc6ae2c8d8d9f1
Christian Poppe <christian.poppe@descript.de>
2020-08-28 20:32:52 +0200

    adds class colour consts

--- raid_roster_planner/raid_roster_drf/constants.py
+++ raid_roster_planner/raid_roster_drf/constants.py
@@ -5,6 +5,31 @@ ROLES = [
     'Melee DD',
 ]
 
-CLASS_COLOURS = [
 
-]
+CLASS_DK = 'death_knight'
+CLASS_DH = 'deamon_hunter'
+CLASS_DRUID = 'druid'
+CLASS_HUNTER = 'hunter'
+CLASS_MAGE = 'mage'
+CLASS_MONK = 'monk'
+CLASS_PALADIN = 'paladin'
+CLASS_PRIEST = 'priest'
+CLASS_ROGUE = 'rogue'
+CLASS_SHAMAN = 'shaman'
+CLASS_WARLOCK = 'warlock'
+CLASS_WARRIOR = 'warrior'
+
+CLASS_COLOURS = {
+    CLASS_DK: '#C41F3B',
+    CLASS_DH: '#A330C9',
+    CLASS_DRUID: '#FF7D0A',
+    CLASS_HUNTER: '#A9D271',
+    CLASS_MAGE: '#40C7EB',
+    CLASS_MONK: '#00FF96',
+    CLASS_PALADIN: '#F58CBA',
+    CLASS_PRIEST: '#FFFFFF',
+    CLASS_ROGUE: '#FFF569',
+    CLASS_SHAMAN: '#0070DE',
+    CLASS_WARLOCK: '#8787ED',
+    CLASS_WARRIOR: '#C79C6E',
+}
