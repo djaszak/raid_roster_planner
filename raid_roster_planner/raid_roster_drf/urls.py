86f02b23cc59360c1912ea6097add221bf4500a8
Christian Poppe <christian.poppe@descript.de>
2020-08-29 01:17:58 +0200

    named view roster

--- raid_roster_planner/raid_roster_drf/urls.py
+++ raid_roster_planner/raid_roster_drf/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     re_path(r'^input/$', views.InputView.as_view(), name='input_form'),
-    re_path(r'^roster/$', views.RosterView.as_view(), name='roster_form')
+    re_path(r'^roster/$', views.RosterView.as_view(), name='roster')
 ]
