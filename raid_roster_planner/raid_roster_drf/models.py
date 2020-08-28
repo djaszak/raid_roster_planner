46ff54895cbb08b13e684fb55ee6f63bc46df5d4
dennis.jaszak <dennis.jaszak@descript.de>
2020-08-28 20:51:28 +0200

    Add requirements and change models to our chosen structure

--- raid_roster_planner/raid_roster_drf/models.py
+++ raid_roster_planner/raid_roster_drf/models.py
@@ -7,15 +7,18 @@ class Player(models.Model):
     name = models.CharField(max_length=255)
 
 
+class Role(models.Model):
+    name = models.CharField(max_length=255)
+
+
 class GameClass(models.Model):
     name = models.CharField(max_length=255)
-    role = models.CharField(max_length=255, choices=constants.ROLES)
-    color = models.CharField(max_length=255, choices=constants.CLASS_COLOURS)
-    icon = models.FileField()
+    colour = models.CharField(max_length=255, choices=constants.CLASS_COLOURS)
 
 
 class Character(models.Model):
     name = models.CharField(max_length=255)
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
-    game_class = models.ManyToManyField(GameClass)
+    game_class = models.ForeignKey(GameClass, on_delete=models.CASCADE)
+    role = models.ManyToManyField(Role)
     armory_link = models.URLField(blank=True)
