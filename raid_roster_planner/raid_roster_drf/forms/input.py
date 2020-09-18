e472a94d87251c32b7f66d2fa4bf978610925333
Christian Poppe <christian.poppe@descript.de>
2020-09-19 00:02:34 +0200

    applied model field changes to input form, fixed missing form error messages

--- raid_roster_planner/raid_roster_drf/forms/input.py
+++ raid_roster_planner/raid_roster_drf/forms/input.py
@@ -1,5 +1,4 @@
 from django import forms
-from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext as _
 
 from raid_roster_planner.raid_roster_drf import models, constants
@@ -24,17 +23,27 @@ class InputForm(forms.Form):
     # character model
     character_name = forms.CharField(label=_('Character Name'))
     game_class = forms.ModelChoiceField(label=_('Class'), queryset=models.GameClass.objects.all())
-    role = forms.ModelMultipleChoiceField(label=_('Role'), queryset=models.Role.objects.all())
+    main_role = forms.ModelChoiceField(label=_('Main Role'), queryset=models.Role.objects.all())
+    off_spec_roles = forms.ModelMultipleChoiceField(label=_('Off-Spec Roles'), queryset=models.Role.objects.all())
     is_main = forms.BooleanField(label=_('This character is your Main.'), required=False)
 
     def clean(self):
         game_class = self.cleaned_data['game_class']
-        roles = self.cleaned_data['role']
+        main_role = self.cleaned_data['main_role']
+        off_spec_roles = self.cleaned_data['off_spec_roles']
 
         possible_roles = constants.CLASS_ROLE_MAPPING[game_class.name]
         possible_roles_human_readable = ', '.join(possible_roles)
 
-        for rl in roles:
+        roles_valid = True
+
+        if main_role not in possible_roles:
+            roles_valid = False
+
+        for rl in off_spec_roles:
             if rl.name not in possible_roles:
-                raise forms.ValidationError(_(f'The class you chose cannot fulfill one of the roles you chose. '
-                                              f'Possible roles for this class are: {possible_roles_human_readable}'))
+                roles_valid = False
+
+        if not roles_valid:
+            raise forms.ValidationError(_(f'The class you chose cannot fulfill one of the roles you chose. '
+                                          f'Possible roles for this class are: {possible_roles_human_readable}'))
